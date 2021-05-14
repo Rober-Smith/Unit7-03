cebc61a17915858935ff998cd5adebc3267568a0
RoberSmith <>
2021-05-14 00:07:25 +0000

    age

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-document.getElementById('button').addEventListener('click',compare)
+document.getElementById('button').addEventListener('click', compare)
 let age = 0
 
-function compare() {
+function compare () {
   age = document.getElementById('input').value
-  age =  parseInt(age)
+  age = parseInt(age)
 
   if (age >= 17) {
-  userInput = document.getElementById('answer').innerHTML = "You can see R rated movies"
+    age = document.getElementById('answer').innerHTML = 
+    'You can see R rated movies'
   } else if (age >= 13) {
-  userInput = document.getElementById('answer').innerHTML = "You can see PG-13 movies"
+    age = document.getElementById('answer').innerHTML = 
+    'You can see PG-13 movies'
   } else {
-  userInput = document.getElementById('answer').innerHTML = "You can see PG and G rated movies"
+    age = document.getElementById('answer').innerHTML = 
+    'You can see PG and G rated movies'
   }
 }
