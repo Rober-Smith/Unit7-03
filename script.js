14125f27e4174fa137ac2014902dcd779f10ecdc
RoberSmith <>
2021-05-14 00:15:57 +0000

    space

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function compare () {
   age = parseInt(age)
 
   if (age >= 17) {
-    age = document.getElementById('answer').innerHTML = 
+    age = document.getElementById('answer').innerHTML =
     'You can see R rated movies'
   } else if (age >= 13) {
-    age = document.getElementById('answer').innerHTML = 
+    age = document.getElementById('answer').innerHTML =
     'You can see PG-13 movies'
   } else {
-    age = document.getElementById('answer').innerHTML = 
+    age = document.getElementById('answer').innerHTML =
     'You can see PG and G rated movies'
   }
 }
